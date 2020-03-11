082c2392108cd2982b7fbec8aed2d77f5656c168
Nathan Nelson <nathan.nelson@acadiasoft.com>
2020-03-11 16:00:58 +0000

    added more basket tests

--- src/Entities/Basket/Basket.test.tsx
+++ src/Entities/Basket/Basket.test.tsx
@@ -1,4 +1,4 @@
-import {addItemToBasket, Basket, createBasket} from './Basket';
+import {addItemsToBasket, addItemToBasket, Basket, createBasket} from './Basket';
 import {Item} from '../Item/Item';
 
 const beans: Item = {productName: 'Beans'};
@@ -14,9 +14,27 @@ describe('Basket - Shopping Basket Methods', () => {
     expect(basket).toMatchObject([]);
   });
 
-  it('Should be able to add an item to a basket', () => {
-    const result = addItemToBasket(beans);
+  it('Should be able to create a basket with items', () => {
+    const match = [beans, beans];
 
-    expect(result).toMatchObject([beans]);
+    basket = createBasket([beans, beans]);
+
+    expect(basket).toMatchObject(match);
+  });
+
+  it('Should be able to add an item to a basket', () => {
+    const match = [beans];
+
+    const result = addItemToBasket(basket, beans);
+
+    expect(result).toMatchObject(match);
+  });
+
+  it('Should be able to add items to a basket', () => {
+    const match = [beans, beans];
+
+    const result = addItemsToBasket(basket, [beans, beans]);
+
+    expect(result).toMatchObject(match);
   });
 });
--- src/Entities/Basket/Basket.tsx
+++ src/Entities/Basket/Basket.tsx
@@ -3,9 +3,13 @@ import {Item} from '../Item/Item';
 export type Basket = Item[];
 
 export const createBasket = (): Item[] => {
-
+  return [];
 };
 
-export const addItemToBasket = (item: Item) => {
+export const addItemToBasket = (basket: Basket, item: Item): Basket => {
+  return [...basket, item];
+};
+
+export const addItemsToBasket = (basket: Basket, items: Item[]): Basket => {
 
 };
