fe9725dd898102f60504477ff1279237dfd96157
Nathan Nelson <nathan.nelson@acadiasoft.com>
2020-03-11 17:18:12 +0000

    completed discount tests, added some more

--- src/Entities/Discount/Discount.test.tsx
+++ src/Entities/Discount/Discount.test.tsx
@@ -1,5 +1,6 @@
 import {Basket, createBasket} from '../Basket/Basket';
 import {Item} from '../Item/Item';
+import {threeForTwo} from './Discount';
 
 const beans: Item = {productName: 'Beans'};
 const coke: Item = {productName: 'Coke'};
@@ -10,7 +11,30 @@ describe('Discounts', () => {
 
     const basket: Basket = createBasket([beans, beans, beans]);
 
-    const result = threeForTwo(basket, 'Beans');
+    const result = threeForTwo('Beans', basket);
+
+    expect(result).toMatchObject(match);
+  });
+
+  it('Three for Two multiple instances', () => {
+    const match = [
+      {label: 'Beans 3 for 2', amount: 0.5},
+      {label: 'Beans 3 for 2', amount: 0.5},
+    ];
+
+    const basket: Basket = createBasket([beans, beans, beans, beans, beans, beans]);
+
+    const result = threeForTwo('Beans', basket);
+
+    expect(result).toMatchObject(match);
+  });
+
+  it('Two for a Pound instances', () => {
+    const match = [{label: 'Coke 2 for £1', amount: 0.4}];
+
+    const basket = createBasket([coke, coke]);
+
+    const result = twoForPound('Coke', basket);
 
     expect(result).toMatchObject(match);
   });
--- src/Entities/Discount/Discount.tsx
+++ src/Entities/Discount/Discount.tsx
@@ -1,3 +1,8 @@
+import {times} from 'ramda';
+
+import {Item} from '../Item/Item';
+import {getProductByName} from '../Product/Product.helper';
+import {ProductByValue} from '../Product/Product';
 import {Basket} from '../Basket/Basket';
 
 export interface Discount {
@@ -5,4 +10,17 @@ export interface Discount {
   amount: number;
 }
 
-export type DiscountFunction = (basket: Basket, productName: string) => Discount[];
+export type DiscountFunction = (productName: string, basket: Basket) => Discount[];
+
+export const threeForTwo: DiscountFunction = (productName, basket) => {
+  const product = getProductByName(productName) as ProductByValue;
+
+  const matchingItems: number = basket.reduce((amount: number, item: Item) => {
+    if (item.productName === productName) return amount + 1;
+
+    return amount;
+  }, 0);
+
+  const sets: number = Math.floor(matchingItems / 3);
+  return times(() => ({label: `${productName} 3 for 2`, amount: product.value}), sets);
+};
